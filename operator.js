381dbb65b882c5a3512bdca549277c0c4c971745
Muhammad Naim <105841108124@student.unismuh.ac.id>
2024-11-18 13:30:23 +0800

    Create operator.js

---
+++ operator.js
@@ -0,0 +1,7 @@
+let nilai = 12
+nilai = nilai + 8 // menambah
+nilai = nilai - 10 // mengurangi
+nilai = nilai / 2 // membagi
+nilai = nilai * 4 // mengkali
+nilai = nilai % 2 // mod
+console.log(nilai); // 0
